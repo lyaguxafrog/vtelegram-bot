394a107eed3098e00e533cb43f49950771954ec1
pr1nce55 <maccree.a@yandex.ru>
2021-08-12 00:28:41 +0300

    function for getting data from vk

--- main.py
+++ main.py
@@ -10,4 +10,10 @@ import bot_config
 
 bot = telebot.TeleBot(bot_config.BOT_TOKEN) # Initializing the telegram bot
 
-
+# Getting data from vk.com
+def get_data(domain_vk, count_vk):
+    vk_session = vk_api.VkApi(bot_config.LOGIN, bot_config.PASSWORD)
+    vk_session.auth()
+    vk = vk_session.get_api()
+    response = vk.wall.get(domain=domain_vk, count=count_vk) # We use the wall method.get from the API documentation vk.com
+    return response
